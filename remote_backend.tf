540e43f7f99938a7f8301ea5af3288bbd9bba336
Pablo Navascues <pablo.navascues@hashicorp.com>
2023-01-03 10:43:24 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "pnavascues-training"
     workspaces {
       name = "hashicat-aws"
